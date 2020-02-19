bb46665c8bf842dc4c8bc078b6085d4aa24ac1df
Karel Peeters <karel.peeters.leuven@gmail.com>
2020-02-19 20:33:57 +0100

    Basic antialiasing, sample 5 times per pixel.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use image::ImageBuffer;
 use nalgebra::{Rotation3, Unit, Vector3};
-use palette::{named, Srgb, Shade};
+use palette::{Alpha, LinSrgba, named, Shade, Srgb, Srgba};
 
 use crate::geometry::{Hit, Plane, Ray, Shape, Sphere};
 use crate::material::Material;
@@ -69,9 +69,9 @@ struct Camera {
 impl Camera {
     //TODO fix distortion on the top and bottom of the image and
     //     this also doesn't work for near-vertical camera directions yet
-    fn ray(&self, width: u32, height: u32, xi: u32, yi: u32) -> Ray {
-        let pitch = self.fov_vertical * ((yi as f32) / (height as f32) - 0.5);
-        let yaw = self.fov_horizontal * ((xi as f32) / (width as f32) - 0.5);
+    fn ray(&self, width: f32, height: f32, xi: f32, yi: f32) -> Ray {
+        let pitch = self.fov_vertical * (yi / height - 0.5);
+        let yaw = self.fov_horizontal * (xi / width - 0.5);
         let rot = Rotation3::from_euler_angles(
             pitch,
             yaw,
@@ -118,18 +118,29 @@ fn main() {
     let width = 1000;
     let height = 1000;
 
-    //TODO multiple rays per pixel for antialiasing
-    let image: ImageBuffer<image::Rgba<u8>, _> = ImageBuffer::from_fn(width, height, |x, y| {
-        let ray = camera.ray(width, height, x, y);
-        let color = scene.trace(&ray, 50);
+    let deltas: [(f32, f32); 5] = [(0.0, 0.0), (1.0, 1.0), (1.0, -1.0), (-1.0, -1.0), (-1.0, 1.0)];
 
-        match color {
-            None => image::Rgba([0, 0, 0, 0]),
-            Some(color) => {
-                let data = Srgb::from_linear(color).into_format();
-                image::Rgba([data.red, data.green, data.blue, 255])
-            }
+    let image: ImageBuffer<image::Rgba<u8>, _> = ImageBuffer::from_fn(width, height, |x, y| {
+        let mut total = LinSrgba::new(0.0, 0.0, 0.0, 0.0);
+
+        for (dx, dy) in &deltas {
+            let ray = camera.ray(
+                width as f32, height as f32,
+                x as f32 + dx / 6.0, y as f32 + dy / 6.0,
+            );
+
+            let color: LinSrgba = scene.trace(&ray, 50)
+                .map(|c| {
+                    Alpha { color: c, alpha: 1.0 }
+                })
+                .unwrap_or(Alpha { color: scene.sky, alpha: 1.0 });
+
+            total += color;
         }
+
+        let average = total / deltas.len() as f32;
+        let data = Srgba::from_linear(average).into_format();
+        image::Rgba([data.red, data.green, data.blue, data.alpha])
     });
     image.save("output.png").unwrap();
 
