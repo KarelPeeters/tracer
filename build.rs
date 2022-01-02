ae57bbe299863a9073345e6827fcc4d9190490e0
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-01-02 14:13:43 +0100

    Update all dependencies (and remove the build script, the windows crate doesn't need it anymore).

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    windows::build!(
-        windows::win32::windows_programming::PROCESS_CREATION_FLAGS,
-        windows::win32::system_services::{GetCurrentProcess, SetPriorityClass},
-    )
-}
